21a78ee62447169e7673400ff11a796c13295885
Marius <marius.sergent@gmail.com>
2025-10-17 16:30:40 +0200

    feat: ajouter des styles globaux pour améliorer l'apparence de l'application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { DynaPuff, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const dynaPuff = DynaPuff({
+  variable: '--font-dynapuff',
   subsets: ['latin']
 })
 
@@ -35,7 +35,7 @@ export default function RootLayout ({
   return (
     <html lang='fr'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dynaPuff.variable} ${geistMono.variable} antialiased font-sans`}
       >
         {children}
       </body>
