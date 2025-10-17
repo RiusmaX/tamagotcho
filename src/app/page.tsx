b23243b2af23a2d60d987b1ddc1fefa08e81dd64
Marius <marius.sergent@gmail.com>
2025-10-17 16:17:46 +0200

    feat: Add web app manifest and update metadata for improved PWA support
    - Added icon1.png for application icons.
    - Updated layout.tsx to include new metadata for the web app, including icons and manifest.
    - Created manifest.json for PWA configuration with app name, icons, and display settings.
    - Enhanced page.tsx metadata for better SEO and social sharing with Open Graph and Twitter card data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,21 @@ import MonstersSection from '@/components/monsters-section'
 import ActionsSection from '@/components/actions-section'
 import NewsletterSection from '@/components/newsletter-section'
 import Footer from '@/components/footer'
+import { Metadata } from 'next'
+
+export const metadata: Readonly<Metadata> = {
+  title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+  description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !',
+  keywords: 'Tamagotcho, monstre virtuel, adoption, jeu, aventure',
+  openGraph: {
+    title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+    description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !'
+  },
+  twitter: {
+    title: 'Tamagotcho - Adopte et prends soin de ton compagnon virtuel',
+    description: 'Tamagotcho est une application web où tu peux adopter, nourrir, jouer et faire évoluer ton propre monstre virtuel. Rejoins-nous pour une aventure amusante et interactive !'
+  }
+}
 
 // Single Responsibility: Home page orchestrates the layout of sections
 export default function Home (): React.ReactNode {
