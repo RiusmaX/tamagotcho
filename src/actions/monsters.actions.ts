1982e45196968c7a11ad10e4476f9580dce9cb41
Marius <marius.sergent@gmail.com>
2025-10-17 15:09:13 +0200

    fix mongoose connexion

--- src/actions/monsters.actions.ts
+++ src/actions/monsters.actions.ts
@@ -1,13 +1,13 @@
 'use server'
 
-import { connectToDatabase } from '@/db'
+import { connectMongooseToDatabase } from '@/db'
 import Monster from '@/db/models/monster.model'
 import { auth } from '@/lib/auth'
 import { CreateMonsterFormValues } from '@/types/forms/create-monster-form'
 import { headers } from 'next/headers'
 
 export async function createMonster (monsterData: CreateMonsterFormValues): Promise<void> {
-  await connectToDatabase()
+  await connectMongooseToDatabase()
 
   const session = await auth.api.getSession({
     headers: await headers()
@@ -27,7 +27,7 @@ export async function createMonster (monsterData: CreateMonsterFormValues): Prom
 
 export async function getMonsters (): Promise<Monster[]> {
   try {
-    await connectToDatabase()
+    await connectMongooseToDatabase()
 
     const session = await auth.api.getSession({
       headers: await headers()
@@ -37,7 +37,7 @@ export async function getMonsters (): Promise<Monster[]> {
     const { user } = session
 
     const monsters = await Monster.find({ ownerId: user.id }).exec()
-    return monsters
+    return JSON.parse(JSON.stringify(monsters))
   } catch (error) {
     console.error('Error fetching monsters:', error)
     return []
